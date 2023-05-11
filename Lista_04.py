a0ec8aa57c1ef2338d6a1e96bd348ee345c30cee
Lucas Ramos Lins Nóbrega <130920104+lucasl1ns@users.noreply.github.com>
2023-05-11 00:47:05 -0300

    Delete Lista_04.py

--- Lista_04.py
+++
@@ -1,4 +0,0 @@
-valor, per = float(input('Preço: R$')), float(input('Percentual do desconto(%): '))
-desconto = (per/100) * valor
-valor_final = valor - desconto
-print(f'Valor final: R${valor_final:.2f}')
