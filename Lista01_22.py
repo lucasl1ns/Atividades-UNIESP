420d422da0ae6503113eb4549f845211978f1493
Lucas Ramos Lins Nóbrega <130920104+lucasl1ns@users.noreply.github.com>
2023-05-18 21:05:34 -0300

    Update Lista01_22.py
    Detectado erro. Antes estava idade.

--- Lista01_22.py
+++ Lista01_22.py
@@ -3,11 +3,15 @@ Desenvolver um algoritmo que leia a altura de 15 pessoas. Este programa deverá 
 a. A menor altura do grupo;
 b. A maior altura do grupo;
 '''
-for i in range (1, 16):
-    altura = int(input('Digite a idade: '))
+for i in range (1, 4):
+    altura = float(input('Digite a altura: '))
     if i == 1:
         maior = altura
+        menor = altura
     else:
         if altura > maior:
             maior = altura
-print(f'A maior idadade é : {maior}')
+        if altura < menor:
+            menor = altura
+print(f'A maior altura é : {maior}')
+print(f'A menor altura é : {menor}')
