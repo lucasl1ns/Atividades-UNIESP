c16b98e3db3e368177ffb78e90e736eeecf47ca8
Lucas Ramos Lins Nóbrega <130920104+lucasl1ns@users.noreply.github.com>
2023-05-18 21:23:44 -0300

    Update Lista01_22.py

--- Lista01_22.py
+++ Lista01_22.py
@@ -3,7 +3,7 @@ Desenvolver um algoritmo que leia a altura de 15 pessoas. Este programa deverá 
 a. A menor altura do grupo;
 b. A maior altura do grupo;
 '''
-for i in range (1, 4):
+for i in range (1, 16):
     altura = float(input('Digite a altura: '))
     if i == 1:
         maior = altura
@@ -15,3 +15,4 @@ for i in range (1, 4):
             menor = altura
 print(f'A maior altura é : {maior}')
 print(f'A menor altura é : {menor}')
+
